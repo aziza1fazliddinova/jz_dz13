e46fe13a423c01be53e4136d65873a91ff6ebd45
aziza <azizafazliddinova08@gmail.com>
2023-10-14 14:06:18 +0500

    firs commit

--- index.js
+++ index.js
@@ -1,10 +1,129 @@
-let pwd 
-let popitki = 0
-do{
- pwd= +prompt('password')
- popitki++
-}while(pwd !==7777 && popitki <3)
+let arr = [
+    {
+        id: Math.random(),
+        name: 'Timur',
+        info: {
+            school: '235',
+            faculity: 'SMM'
+        }
+    },
+    {
+        id: Math.random(),
+        name: 'Imran',
+        info: {
+            school: 'ne izvestno',
+            faculity: 'programming'
+        }
+    },
+    {
+        id: Math.random(),
+        name: 'Aminjon',
+        info: {
+            school: '444',
+            faculity: 'Dizayn'
+        }
+    },
+    {
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        }
+    },
+    {
+        id: Math.random(),
+        name: 'Muxammad',
+        info: {
+            school: '5555',
+            faculity: 'Backend'
+        }
+    },
+    {
+        id: Math.random(),
+        name: 'Timur',
+        info: {
+            school: '235',
+            faculity: 'SMM'
+        }
+    },
+    {
+        id: Math.random(),
+        name: 'Imran',
+        info: {
+            school: 'ne izvestno',
+            faculity: 'programming'
+        }
+    },
+    {
+        id: Math.random(),
+        name: 'Aminjon',
+        info: {
+            school: '444',
+            faculity: 'Dizayn'
+        }
+    },
+    {
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        }
+    },
 
-console.log(pwd);
+    {
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        }
+    },
+    {
+        id: Math.random(),
+        name: 'Muxammad',
+        info: {
+            school: '5555',
+            faculity: 'Backend'
+        }
+    }
+]
 
+let categories = [
+    {
+        course: ' SMM',
+        count: 0
+    },
+    {
+        course: 'PROGRAMMING',
+        count: 0
+    },
+    {
+        course: '     3DSMAX',
+        count: 0
+    },
+    {
+        course: ' DIZAYN',
+        count: 0
+    },
+    {
+        course: '   BACKEND',
+        count: 0
+    },
+]
 
+for (let i = 0; i < arr.length;
+    i++) {
+    const student = arr[i];
+    const faculty = student.info.faculity.trim().toUpperCase();
+    const category = categories.find(categ => categ.course.trim() === faculty);
+    if (category) {
+        category.count++;
+    }
+}
+
+console.table(categories);
+
+//Метод find() возвращает значение первого найденного в массиве элемента, 
+//которое удовлетворяет условию переданному в callback функции. В противном случае возвращается undefiend
